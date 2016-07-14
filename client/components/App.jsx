2d9ebbabfb91d9bb1e9fc7999bda5f5f28f6d081
Svyatoslav Ovsyannikov <optatt@yandex.ru>
2016-07-14 03:31:20 +0300

    task 1 process

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,6 +6,17 @@ import './../style/style.less';
 
 export default class App extends Component{
     render() {
+
+		function dscount(string, target) {
+			var result = string.match(target);
+			console.log(result.length);
+		}
+
+		var reg = /s1|s2?=s1|s2{1,}/ig;
+		var string = 's2';
+		dscount(string, reg)
+
+
         return (
             <div className='App'>
                 <UserList />
